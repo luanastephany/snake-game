206a2c61086fd4ef82c1bae6924c4cc8640ae18d
Luana <ls.luanastephany@gmail.com>
2021-05-19 09:59:23 -0300

    cobrinha passando pelas paredes

--- script.js
+++ script.js
@@ -23,15 +23,23 @@ function criarCobrinha() {
 }
 
 document.addEventListener('keydown', update)
-
+//fazendo a cobrinha andar para todos os lados
 function update(event) {
-  if (event.keyCode == 37 && direction != 'right') direction = 'left'
-  if (event.keyCode == 38 && direction != 'down') direction = 'up'
-  if (event.keyCode == 39 && direction != 'left') direction = 'right'
-  if (event.keyCode == 40 && direction != 'up') direction = 'down'
+  if (event.code == 'ArrowLeft' && direction != 'right') direction = 'left'
+  if (event.code == 'ArrowUp' && direction != 'down') direction = 'up'
+  if (event.code == 'ArrowRight' && direction != 'left') direction = 'right'
+  if (event.code == 'ArrowDown' && direction != 'up') direction = 'down'
 }
 
 function iniciarJogo() {
+
+  //cobrinha passando pelas paredes
+  if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0
+  if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box
+  if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0
+  if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box
+
+
   criarBG()
   criarCobrinha()
 
